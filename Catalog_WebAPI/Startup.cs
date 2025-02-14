1794e8c7a0bc94e8a2734b616f2c20052675a7cb
Денис Богомолов <31684886+den3011den@users.noreply.github.com>
2025-02-14 21:02:39 +0700

    Финальный коммит

--- Catalog_WebAPI/Startup.cs
+++ Catalog_WebAPI/Startup.cs
@@ -3,7 +3,10 @@ using Catalog_Business.Repository.IRepository;
 using Catalog_Common;
 using Catalog_DataAccess;
 using Catalog_DataAccess.DbInitializer;
+using Catalog_WebAPI.Controllers;
 using Microsoft.EntityFrameworkCore;
+using Swashbuckle.AspNetCore.SwaggerUI;
+using System.Reflection;
 using static Catalog_Common.SD;
 using IConfiguration = Microsoft.Extensions.Configuration.IConfiguration;
 
@@ -27,7 +30,39 @@ namespace Catalog_WebAPI
                 x.SuppressAsyncSuffixInActionNames = false);
 
             services.AddEndpointsApiExplorer();
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(c =>
+            {
+                // ...other stuff
+
+                // include API xml documentation
+                var apiAssembly = typeof(AuthorsController).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(apiAssembly));
+
+                apiAssembly = typeof(PublishersController).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(apiAssembly));
+
+                apiAssembly = typeof(StatesController).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(apiAssembly));
+
+                // include models xml documentation
+                var modelsAssembly = typeof(Catalog_Models.CatalogModels.Author.AuthorItemCreateUpdateRequest).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.Author.AuthorItemResponse).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.Publisher.PublisherItemCreateUpdateRequest).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.Publisher.PublisherItemResponse).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.State.StateItemCreateRequest).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.State.StateItemResponse).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+                modelsAssembly = typeof(Catalog_Models.CatalogModels.State.StateItemUpdateRequest).Assembly;
+                c.IncludeXmlComments(GetXmlDocumentationFileFor(modelsAssembly));
+
+            });
 
             services.AddScoped<IDbInitializer, DbInitializer>();
             services.AddScoped<IStateRepository, StateRepository>();
@@ -86,11 +121,11 @@ namespace Catalog_WebAPI
                     }
             }
 
-            //services.AddOpenApiDocument(options =>
-            //{
-            //    options.Title = "Catalog (Library API Doc)";
-            //    options.Version = "1.0";
-            //});
+            services.AddOpenApiDocument(options =>
+            {
+                options.Title = "Catalog (Library API Doc)";
+                options.Version = "1.0";
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -107,13 +142,14 @@ namespace Catalog_WebAPI
             //    app.UseHsts();
             //}
 
-            //app.UseOpenApi();         
+            app.UseOpenApi();
 
-            //app.UseSwaggerUI(x =>
-            //{
-            //    //x.DocExpansion = "list";
+            app.UseSwaggerUI(x =>
+            {
+                x.DocExpansion(DocExpansion.List);
+            });
+
             //    x.SwaggerEndpoint("/openapi/v1.json", "Catalog API ver. 1");
-            //});
 
             app.UseHttpsRedirection();
 
@@ -125,6 +161,14 @@ namespace Catalog_WebAPI
             });
 
             dbInitializer.InitializeDb();
+        }
+
+        private static string GetXmlDocumentationFileFor(Assembly assembly)
+        {
+            var documentationFile = $"{assembly.GetName().Name}.xml";
+            var path = Path.Combine(AppContext.BaseDirectory, documentationFile);
+
+            return path;
         }
     }
 }
